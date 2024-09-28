35b27825f80742b8efac93fa23da029af277dc9a
EC2 Default User <ec2-user@ip-172-31-47-153.us-east-2.compute.internal>
2024-09-28 19:10:50 +0000

    search bar update

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     path('', podcast.loadmain, name='loadmain'),  
     path('load_podcast/', podcast.load_podcast, name='load_podcast'),
+    path('searchPodcasts/', podcast.searchPodcasts, name='searchPodcasts'),
 ]
 
