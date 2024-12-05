79d7de5948f0bbc690bc5ff3a156bc51df930e61
EC2 Default User <ec2-user@ip-172-31-47-153.us-east-2.compute.internal>
2024-12-05 21:09:07 +0000

    deploy debug

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseRedirect
-from django.urls import include, path, re_path
-from django.views.static import serve
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,8 +10,4 @@ urlpatterns = [
     
     # Include the URLs from myapp
     path('myapp/podcast/', include('myapp.urls')),  
-]
-
-static_urlpatterns = [
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
