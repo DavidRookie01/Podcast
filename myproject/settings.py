ab29498f8b85726a8222b752eea7ca581b11f309
EC2 Default User <ec2-user@ip-172-31-47-153.us-east-2.compute.internal>
2024-12-05 19:05:53 +0000

    deploy debug

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/myapp/static/'  # This is likely already present.
+STATIC_URL = 'static/'  # This is likely already present.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
