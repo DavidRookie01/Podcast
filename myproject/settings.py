876fca22ca6efbf81b3a09a5c91a111823f5ac28
EC2 Default User <ec2-user@ip-172-31-47-153.us-east-2.compute.internal>
2024-12-06 23:40:56 +0000

    Update settings.py for whitenoise

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'  # This is likely already present.
+STATIC_URL = 'static/'  # This is likely already present.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
